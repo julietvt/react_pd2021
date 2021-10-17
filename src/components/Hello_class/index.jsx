478805baa56aa764c22b550a9a4efbe1d0b33555
julietvt <freshitpt@gmail.com>
2021-10-17 10:38:42 +0300

    class and fun component hello

---
+++ src/components/Hello_class/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {Component} from 'react';
+
+class HelloClass extends Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return <div>Hello, React!!</div>;
+    }
+}
+export default HelloClass;
+
---
+++ src/components/Hello_fun/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+const HelloFun = (props) => {
+  return <div>Hello, React!!</div>;
+};
+
+export default HelloFun;
