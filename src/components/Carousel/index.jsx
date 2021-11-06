5d41235d0b995439cb57626b99a001d4397160f1
julietvt <freshitpt@gmail.com>
2021-11-06 11:15:24 +0200

    Carousel. main index was updated

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -8,24 +8,57 @@ export default class Carousel extends Component {
         this.state = {
             index: 0,
             width: 0,
-            height: 0
+            height: 0,
+            timerId: null,
         };
     };
-    setFullScreenMode = (isFullScreen) => {}
-    setNextImage= ()=> {}
-    setPrevImage= ()=> {}
-    render() {
-        const {index} = this.state;
-        const {slides} = this.props;
-        return (
-            <div>
-                <Slide currentImage={slides[index]} />
-                <Slider next={this.setNextImage} prev={this.setPrevImage}  setFullScreenMode={this.setFullScreenMode} />                
-            </div>
-        );
+     setFullScreenMode = (isFullScreen) => {
+    !isFullScreen
+      ? this.setState({ width: "50%", height: "50%" })
+      : this.setState({ width: 300, height: 300 });
+  };
+
+  start = () => {
+    if (!this.timerId) {
+      this.timerId = setInterval(this.setNext, 2000);
     }
+  };
+  stop = () => {
+    clearInterval(this.timerId);
+    this.timerId = null;
+  };
+  startPlay = (isPlay) => {
+    !isPlay ? this.start() : this.stop();
+  };
+
+  setNext = () => {
+    const { index } = this.state;
+    const { slides } = this.props;
+    console.log(slides.length);
+    this.setState({ index: (index + 1) % slides.length });
+  };
+  setPrev = () => {
+    const { index } = this.state;
+    const { slides } = this.props;
+    console.log(slides.length);
+    this.setState({ index: (index - 1 + slides.length) % slides.length });
+  };
+
+  render() {
+    const { index, width, height } = this.state;
+    const { slides } = this.props;
+    return (
+      <div>
+        <Slide currentImage={slides[index]} width={width} height={height} />
+        <Slider
+          next={this.setNext}
+          prev={this.setPrev}
+          setFullScreenMode={this.setFullScreenMode}
+          startPlay={this.startPlay}
+        />
+      </div>
+    );
+  }
 }
 
-//window
-//window.innerHeight
-//window.innerWidth
+
