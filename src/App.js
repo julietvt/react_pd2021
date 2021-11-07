f4fe58f1b4c0166e5092d1852556e2444479dcad
julietvt <freshitpt@gmail.com>
2021-11-07 11:05:30 +0200

    clock component with hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
 import React, { Component } from 'react';
 import ClockMatrix from './components/ClockMatrix';
-import DivRender from './components/DivRender';
+//import DivRender from './components/DivRender';
 
 const App = () => {
   return (
     <div>
       <ClockMatrix />
-      <DivRender />
     </div>
   );
 };
--- src/components/ClockMatrix/index.jsx
+++ src/components/ClockMatrix/index.jsx
@@ -1,8 +1,34 @@
+import { addMilliseconds, format } from "date-fns";
 import React, {useState, useEffect} from "react";
 import styles from './ClockMatrix.module.scss';
 
 function ClockMatrix(props){
-    return ();
+    const [time, setTime] = useState(new Date(0,0,0,0,0,0));
+    const [isRun, setIsRun] = useState(false);    
+    const handleReset = () => {
+        setTime(new Date(0,0,0,0,0,0));
+    };
+    const handleRunning = () => {
+        setIsRun(!isRun);
+    }
+    useEffect(() => {
+        if(isRun){
+            const idTimer = setInterval(()=>{
+                setTime(addMilliseconds(time,1000));
+            },1000);
+            return () => {
+                clearInterval(idTimer);
+            }
+        }
+    });
+    return (
+        <div className={styles.bodyClock}>
+            <h2 className={styles.matrixDigits}>{format(time, 'HH:mm:ss')}</h2>
+            <button className={styles.btn} onClick={handleRunning}>
+                {isRun ? 'Stop' : 'Start'}
+            </button>
+            <button className={styles.btn} onClick={handleReset}>Reset</button>
+        </div>
+    );
 }
-
 export default ClockMatrix;
